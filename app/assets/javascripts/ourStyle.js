8f6ba02bdfa8823b8d12b682d09603d601b164e4
Bernhard <bernhard.ploder@gmail.com>
2019-06-04 21:46:00 +0200

    added AWESOME visual effects

--- app/assets/javascripts/ourStyle.js
+++ app/assets/javascripts/ourStyle.js
@@ -34,12 +34,33 @@ function openButtonMobileMenu(element) {
     //body.style.paddingTop = header.style.height;
 }
 
+
+
+/*
+let coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}*/
+
+
 function openCollapsible(element) {
     let content = element.nextElementSibling;
     if (content.style.maxHeight){
         content.style.maxHeight = null;
+        element.classList.remove("active")
     } else {
         content.style.maxHeight = content.scrollHeight + "px";
+        element.classList.add("active")
     }
 }
 
