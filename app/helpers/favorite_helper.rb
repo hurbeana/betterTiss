e7bea1f2c555406add7a6562ec07e65dc612da81
Bernhard <bernhard.ploder@gmail.com>
2019-06-08 18:39:15 +0200

    added documentation comments to favorite helper

--- app/helpers/favorite_helper.rb
+++ app/helpers/favorite_helper.rb
@@ -1,4 +1,10 @@
+##
+# Helper for favorite objects
 module FavoriteHelper
+
+  ##
+  # Used to check whether an object can be a favorite of a user or not.
+  # @param obj The object that needs to be checked for favorite functionality.
   def favorite?(obj)
     ass = obj.class.name.pluralize.downcase
     obj.id.nil? ? false : current_user.send(ass).exists?(obj.id)
