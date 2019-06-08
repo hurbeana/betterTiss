65402cea4ae8add76ceb0c9e3d8214fd83f8c6a2
Bernhard <bernhard.ploder@gmail.com>
2019-06-08 18:38:59 +0200

    added documentation comments to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,13 @@
+##
+# Used to provide the full site title and the base tiss link.
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "BetterTiss"
+    base_title = 'BetterTiss'
     if page_title.empty?
       base_title
     else
-      page_title + " | " + base_title
+      page_title + ' | ' + base_title
     end
   end
 
